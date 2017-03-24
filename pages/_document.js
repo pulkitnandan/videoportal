ebf98a4c7521c72bdc8d48190d3b6f9827880dab
Rohit Nandan <pulkitnandan@gmail.com>
2017-03-24 18:19:04 +0530

    FIX local boostrap

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+             <link rel="stylesheet" href="/static/bootstrap.min.css" />
         </Head>
         <body>
         <div  className="container">
